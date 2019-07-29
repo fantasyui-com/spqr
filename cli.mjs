4a80f4738b795b92dd152bed7552bba7bec6d64f
Captain Fantasy <fantasyui.com@gmail.com>
2019-07-29 18:59:12 -0400

    Action Decouple

--- cli.mjs
+++ cli.mjs
@@ -1,9 +1,34 @@
 #!/usr/bin/env -S node --experimental-modules
 
+import {inspect} from 'util';
 import program from 'commander';
 import path from 'path';
 import fs from 'fs';
 import spqr from './index.mjs';
+
+import kebabCase from 'lodash/kebabCase.js';
+import camelCase from 'lodash/camelCase.js';
+
+const actionInstaller = async function({name,context}){
+  const system = JSON.parse( (await fs.promises.readFile('./actions.json')).toString() );
+  const action = system.program.actions.filter(action=>camelCase(action.configuration.name)===camelCase(name)).pop();
+  console.log(action.data)
+
+  for(let {name} of action.data){
+    const id = camelCase(name);
+    if(!context[id]) throw new Error(`Action context requires: ${id}`)
+  }
+
+  for(let element of action.actions){
+    element.context = {};
+    for(let item of element.data){
+      const id = camelCase(item.name);
+      element.context[id] = context[id];
+    }
+  }
+
+  return action;
+}
 
 async function main(){
 
@@ -17,16 +42,27 @@ async function main(){
   program
     .command('new <name>')
     .option('-t, --template [name]', 'Specify template [drafonfly]', 'drafonfly')
-    .action(function (name, {template}) {
+    .action(async function (name, {template}) {
       const root = path.resolve('.');
 
-      const actions = [
-        {type:'new-create-dirs', name, root},
-        {type:'new-create-package', name, root},
-        {type:'new-apply-template', name, root, template},
-      ];
-      console.log(actions);
-      spqr({actions});
+      const actions = await actionInstaller({
+        name:'New Site',
+        context:{
+          siteName: name,
+          siteRootPath: root,
+          templateName: template,
+        }
+      });
+
+
+      // const actions = [
+      //   {type:'new-create-dirs', name, root},
+      //   {type:'new-create-package', name, root},
+      //   {type:'new-apply-template', name, root, template},
+      // ];
+      console.log(inspect(actions, null, 3));
+
+      //spqr({actions});
 
   });
 
